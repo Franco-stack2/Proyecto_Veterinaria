e94438944d76c3628b8d796c343bc5c9f62e403d
Franco <14francosmm@gmail.com>
2026-08-01 15:55:16 -0600

    Version inicial del proyecto

--- src/Vista/FrmFormularioRegistro.java
+++ src/Vista/FrmFormularioRegistro.java
@@ -43,7 +43,7 @@ public class FrmFormularioRegistro extends javax.swing.JFrame {
         cmbTipoUsuario = new javax.swing.JComboBox<>();
         btnRegistrarUsuario = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel1.setText("Registro  ");
