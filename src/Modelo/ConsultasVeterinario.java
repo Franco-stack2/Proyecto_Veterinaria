e7f32938999929d5845fb5a80f7cd222628ecee7
Kerryhub16 <kalfaro40767@ufide.ac.cr>
2026-08-01 23:38:45 -0600

    arreglitos

--- src/Modelo/ConsultasVeterinario.java
+++ src/Modelo/ConsultasVeterinario.java
@@ -10,10 +10,7 @@ import java.sql.*;
  *
  * @author alfar
  */
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ConsultasVeterinario extends Conexion {
 
