0d3d42b04e5b436d8d884a5ad0ec52b097556a9c
Kerryhub16 <kalfaro40767@ufide.ac.cr>
2026-08-01 23:27:48 -0600

    dispose

--- src/Vista/FrmConsultarHistorial.java
+++ src/Vista/FrmConsultarHistorial.java
@@ -44,7 +44,7 @@ public class FrmConsultarHistorial extends javax.swing.JFrame {
         txtAlergias = new javax.swing.JTextField();
         txtCondicionesPasadas = new javax.swing.JTextField();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
